f1f24964c77ca08a3fc1e15c40935f42f1c673b2
Michiel Koning <mail@michielkoning.nl>
2018-03-30 13:17:59 +0200

    update setup

---
+++ includes/admin.php
@@ -0,0 +1,33 @@
+<?php
+function contact__show_mails(){
+  $type = (isset($_GET['type'])) ? $_GET['type'] : null;
+  ?>
+  <div class="wrap">
+    <h2>Maillogs</h2>
+
+    <?php contact__show_list($type); ?>
+  </div>
+  <?php
+}
+
+function contact__show_list($type = ''){
+  global $wpdb;
+
+  $wpdb->contact = $wpdb->prefix . 'contact';
+  $where = ($type) ? "WHERE type = '$type'" : '';
+
+  $contactlist = $wpdb->get_results("SELECT * FROM $wpdb->contact $where ORDER BY enterdate DESC");
+  foreach ($contactlist as $contactitem) {
+    ?>
+    <a href="#" onclick="jQuery('#contact-<?php echo $contactitem->id; ?>').slideToggle();return false"><strong><?php echo $contactitem->name; ?></strong></a> (<?php echo mysql2date('j F Y H:i',$contactitem->enterdate); ?>)<br />
+    <div id="contact-<?php echo $contactitem->id; ?>" style="display:none;">
+      <?php echo $contactitem->body; ?>
+    </div>
+  <?php
+  }
+}
+
+add_action('admin_menu', 'add_contact_block');
+function add_contact_block() {
+  add_menu_page( 'Maillogs', 'Maillogs', 'edit_pages', 'contact_handle', 'contact__show_mails', 'dashicons-email-alt');
+}
--- index.php
+++ index.php
@@ -11,174 +11,31 @@ Version: 1.0
 Author URI: http://www.michielkoning.nl
 */
 
-//Tabel aanmaken voor de contact
-function contact_install () {
-   	global $wpdb;
+require_once( __DIR__ . '/includes/install.php');
+require_once( __DIR__ . '/includes/admin.php');
 
-	// Check for capability
-	if ( !current_user_can('activate_plugins') )
-		return;
 
-	// upgrade function changed in WordPress 2.3
-	require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
-
-	// add charset & collate like wp core
-	$charset_collate = '';
-
-	if ( version_compare(mysql_get_server_info(), '4.1.0', '>=') ) {
-		if ( ! empty($wpdb->charset) )
-			$charset_collate = "DEFAULT CHARACTER SET $wpdb->charset";
-		if ( ! empty($wpdb->collate) )
-			$charset_collate .= " COLLATE $wpdb->collate";
-	}
-
-	$tbl_contact = $wpdb->prefix . 'contact';
-
-	if($wpdb->get_var("show tables like '$tbl_contact'") != $tbl_contact) {
-
-		$sql = "CREATE TABLE " . $tbl_contact . " (
-		id BIGINT(20) NOT NULL AUTO_INCREMENT ,
-		type VARCHAR(255) DEFAULT '' NOT NULL ,
-		name VARCHAR(255) DEFAULT '' NOT NULL ,
-		body text NULL ,
-		enterdate timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-		PRIMARY KEY id (id)
-		) $charset_collate;";
-		dbDelta($sql);
-	}
-}
-
-//Bij het activeren van de plugin wordt de installatie functie aangeroepen
-register_activation_hook(__FILE__, 'contact_install' );
-
-function show_mails(){
-	$type = (isset($_GET['type'])) ? $_GET['type'] : null;
-	?>
-	<div class="wrap">
-		<h2>Maillogs</h2>
-
-		<div class="tablenav top">
-			<div class="alignleft actions">
-				<form method="get">
-					<input type="hidden" name="page" value="contact_handle" />
-					<select name="type">
-						<option value="">Toon alle reacties</option>
-						<option value="contact" <?php if ($type == 'contact') echo 'selected="selected"'; ?>>Contact</option>
-						<option value="ondernemingsrecht" <?php if ($type == 'ondernemingsrecht') echo 'selected="selected"'; ?>>Ondernemingsrecht</option>
-						<option value="arbeidsrecht" <?php if ($type == 'arbeidsrecht') echo 'selected="selected"'; ?>>Arbeidsrecht</option>
-						<option value="bestuursrecht" <?php if ($type == 'bestuursrecht') echo 'selected="selected"'; ?>>Bestuursrecht</option>
-						<option value="strafrecht" <?php if ($type == 'strafrecht') echo 'selected="selected"'; ?>>Strafrecht</option>
-						<option value="personen-+en+familierecht" <?php if ($type == 'personen-+en+familierecht') echo 'selected="selected"'; ?>>Personen- en familierecht</option>
-
-					</select>
-					<input type="submit" value="Filter" class="button-secondary" id="post-query-submit" name="">
-				</form>
-			</div>
-			<br class="clear">
-		</div>
-
-		<br />
-		<?php show_list_mails($type); ?>
-	</div>
-	<?php
-}
-
-function show_list_mails($type = ''){
+function contact__add($message, $type, &$name = '') {
 	global $wpdb;
 
-	$wpdb->contact = $wpdb->prefix . 'contact';
-	$where = ($type) ? "WHERE type = '$type'" : '';
+	$result = $wpdb->insert(
+		$wpdb->prefix . 'contact',
+		array(
+			'type'    => $type,
+			'name'    => $name,
+			'body'    => $message
+		),
+		array(
+			'%s',
+			'%s',
+			'%s'
+		)
+	);
 
-	$contactlist = $wpdb->get_results("SELECT * FROM $wpdb->contact $where ORDER BY enterdate DESC");
-	foreach ($contactlist as $contactitem) {
-		?>
-		<a href="#" onclick="jQuery('#contact-<?php echo $contactitem->id; ?>').slideToggle();return false"><strong><?php echo $contactitem->name; ?></strong></a> (<?php echo mysql2date('j F Y H:i',$contactitem->enterdate); ?>)<br />
-		<div id="contact-<?php echo $contactitem->id; ?>" style="display:none;">
-			<?php echo $contactitem->body; ?>
-		</div>
-	<?php
-	}
+	return $result;
 }
 
-add_action('admin_menu', 'add_contact_block');
-function add_contact_block() {
-	add_menu_page( 'Maillogs', 'Maillogs', 'edit_pages', 'contact_handle', 'show_mails', 'dashicons-email-alt');
-}
-
-add_action( 'wp_ajax_submit_ajax_form', 'submit_ajax_form' );
-add_action( 'wp_ajax_nopriv_submit_ajax_form', 'submit_ajax_form' );
-
-//Indien in frontend, roep de function update_newsletter op, zodra post[newsletter] gevuld is
-function submit_ajax_form() {
-
-	check_ajax_referer( 'submit-form', 'security' );
-
-	$fields  = array();
-
-	$fields[] = array(
-		'label' => 'Naam',
-		'value' =>  $_POST['contact-name']
-	);
-
-	$fields[] = array(
-		'label' => 'E-mail',
-		'value' =>  $_POST['contact-email']
-	);
-
-	$fields[] = array(
-		'label' => 'Opmerkingen',
-		'value' =>  $_POST['contact-comments']
-	);
-
-	$fields[] = array(
-		'label' => 'Formulier',
-		'value' =>  $_POST['formkey']
-	);
-
-	$fullname = $_POST['contact-name'];
-	$subject = 'Reactie via website ' . get_bloginfo('url');
-
-	$message =	'Er is een reactie binnengekomen op <a href="' . get_bloginfo('url') . '">' . get_bloginfo('url') . '</a>.
-	<br /><br />
-	De volgende gegevens zijn ingevuld:
-	<br /><br />
-	<table width="100%">';
-	foreach ($fields as $field) {
-		$message .= '<tr><td width="150">' . $field['label'] . '</td><td>' . $field['value'] . '</td></tr>';
-	}
-	$message .= '<tr><td colspan="2">&nbsp;</td></tr>
-	</table>';
-
-	contact_send_mail($message, $subject, $_POST['formkey'], $log = true, $fullname);
-
-	echo '<p>Hartelijk dank voor uw reactie. Wij nemen zo snel mogelijk contact met u op.</p>';
-
-	if (isset($_POST['ajax'])) {
-		die();
-	}
-}
-
-function contact_send_mail($message, $subject, $type, $log = false, &$name = '', &$to = '', &$cc = false) {
-
-	if ($log) {
-		global $wpdb;
-
-		$result = $wpdb->insert(
-			$wpdb->prefix . 'contact',
-			array(
-				'type'    => $type,
-				'name'    => $name,
-				'body'    => $message
-			),
-			array(
-				'%s',
-				'%s',
-				'%s'
-			)
-		);
-
-		if (!$result) return;
-	}
+function contact__send_mail($message, $subject, $type, &$name = '', &$to = '', &$cc = false) {
 
 	//de mailtemplate
 	$mailbody =
@@ -206,14 +63,14 @@ function contact_send_mail($message, $subject, $type, $log = false, &$name = '',
 	</html>';
 
 	if ($to == '')
-		$to = MAILTO;
+		$to = get_field('email', 'option');
 
 	if ($cc)
-	    $to .= ',' . $cc;
+		$to .= ',' . $cc;
 
 	$mailheaders = "MIME-Version: 1.0\n" .
-		"From: michielkoning@gmail.com\n" .
+		"From: " . get_field('email', 'option') . "\n" .
 		"Content-Type: text/html; charset=\"" . get_bloginfo('charset') . "\"\n";
 
-	mail($to, $subject, $mailbody, $mailheaders);
+	wp_mail($to, $subject, $mailbody, $mailheaders);
 }
